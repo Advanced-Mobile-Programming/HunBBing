4a6bc25c4ea994fad07ced3bc09a896ea4bb5ca3
jiwon <pjw99032986@gmail.com>
2023-11-05 10:44:59 +0900

    리스트 액티비티 추가

---
+++ app/src/main/java/com/example/hunbbing/SellListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hunbbing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SellListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sell_list)
+    }
+}
