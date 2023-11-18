a08547469d9394a988843d39fbcb09506f61fd3c
junhaa <wooha34567@gmail.com>
2023-11-18 23:15:50 +0900

    11/18 사용자 정보 데이터베이스 저장 함수 추가

--- app/src/main/java/com/example/hunbbing/SignUpActivity.kt
+++ app/src/main/java/com/example/hunbbing/SignUpActivity.kt
@@ -4,24 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.hunbbing.Entity.User
 import com.example.hunbbing.databinding.ActivitySignupBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.util.Date
+import kotlin.random.Random
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
     private lateinit var firebaseAuth: FirebaseAuth
-
+    private lateinit var database: DatabaseReference // realtime firebase reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
-
+        database = FirebaseDatabase.getInstance().reference
         binding.signupButton.setOnClickListener{
             val email = binding.signupEmail.text.toString()
             val password = binding.signupPassword.text.toString()
+            // user 데이터에 이름과 생년월일 추가
+            val name = binding.signupName.text.toString()
+            val birth = binding.signupBirth.text.toString()
 
             if (email.isNotEmpty() && password.isNotEmpty()){
                 firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
@@ -29,6 +37,7 @@ class SignUpActivity : AppCompatActivity() {
                         val intent = Intent(this, LoginActivity::class.java)
                         startActivity(intent)
                     } else {
+                        println(it.exception.toString());
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                     }
                 }
@@ -40,5 +49,10 @@ class SignUpActivity : AppCompatActivity() {
             val loginIntent = Intent(this, LoginActivity::class.java)
             startActivity(loginIntent)
         }
+    }
+
+    // 데이터베이스에 유저 정보 저장
+    fun saveUser(uid: String, email: String, name: String, birth: String){
+        database.child("user").child(uid).setValue(User(email,name,birth))
     }
 }
