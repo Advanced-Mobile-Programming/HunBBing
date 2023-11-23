d485ef4ae70d9687a6fb064367c00bfeaad29199
junhaa <2171326@hansung.ac.kr>
2023-11-24 05:02:42 +0900

    채팅방 ID -> 채팅내역 ID로 변경 (child 수정)

--- app/src/main/java/com/example/hunbbing/Entity/ChatRoom.kt
+++ app/src/main/java/com/example/hunbbing/Entity/ChatRoom.kt
@@ -1,7 +1,7 @@
 package com.example.hunbbing.Entity
 
 data class ChatRoom(
-    val chatRoomId: String, // 채팅방 ID
+    val chatId: String, // 채팅내역 ID
     var chatUserName : String, // 채팅 상대방의 이름
     val lastChat : String
 )
