94748a7ee8bddfb6bed9d74ba32317d9c9d76177
jiwon <pjw99032986@gmail.com>
2023-11-24 20:28:02 +0900

    11월11일 검색기능, 검색필터링 추가

---
+++ app/src/main/java/com/example/hunbbing/Look.kt
@@ -0,0 +1,11 @@
+package com.example.hunbbing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Look : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_look)
+    }
+}
