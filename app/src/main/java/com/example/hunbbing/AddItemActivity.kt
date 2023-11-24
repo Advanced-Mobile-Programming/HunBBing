224956882db37cd60d39ddd7053cddd3808988df
rlaekthf <100848728+rlaekthf@users.noreply.github.com>
2023-11-24 16:42:03 +0900

    카메라 권한 실패

--- app/src/main/java/com/example/hunbbing/AddItemActivity.kt
+++ app/src/main/java/com/example/hunbbing/AddItemActivity.kt
@@ -3,6 +3,7 @@ package com.example.hunbbing
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -13,6 +14,7 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import java.util.UUID
+import android.Manifest
 
 
 class AddItemActivity : AppCompatActivity() {
@@ -35,21 +37,6 @@ class AddItemActivity : AppCompatActivity() {
         setContentView(R.layout.activity_item_add)
 
 
-        DbR = FirebaseDatabase.getInstance().getReference("AddItems")
-
-        plusbtn.setOnClickListener {
-            if (selectedImgUri != null) {
-                uploadImageToFirebaseStorage()
-            } else {
-                Toast.makeText(this, "이미지를 선택해주세요", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        addImgbtn.setOnClickListener {
-            selectImage()
-        }
-
-
         item = findViewById(R.id.itemText)
         price = findViewById(R.id.priceText)
         explain = findViewById(R.id.explainText)
@@ -58,10 +45,34 @@ class AddItemActivity : AppCompatActivity() {
         addImgbtn = findViewById(R.id.addImgBtn)
         plusbtn = findViewById(R.id.plusbtn)
 
+
+        DbR = FirebaseDatabase.getInstance().getReference("AddItems")
+
+        plusbtn.setOnClickListener {
+            /*
+            if (selectedImgUri != null) {
+                uploadImageToFirebaseStorage()
+            } else {
+                Toast.makeText(this, "이미지를 선택해주세요", Toast.LENGTH_SHORT).show()
+            }
+            */
+
+            addItemToDatabase()
+        }
+
+        addImgbtn.setOnClickListener {
+            requestStoragePermission()
+            //selectImage()
+        }
+
+
+
+
     }
 
+    //imageUrl: String , imageUrl
 
-    private fun addItemToDatabase(imageUrl: String) {
+    private fun addItemToDatabase() {
         val itemName = item.text.toString()
         val itemPrice = price.text.toString().toInt()
         val itemPriceText = price.text.toString()
@@ -70,11 +81,14 @@ class AddItemActivity : AppCompatActivity() {
 
         if (itemName.isEmpty() || itemPriceText.isEmpty() || itemexplain.isEmpty()  != null) {
             val itemId = DbR.push().key
-            val newItem = Item(itemId, itemName, itemPrice, itemexplain, itemTags, imageUrl)
+            val newItem = Item(itemId, itemName, itemPrice, itemexplain, itemTags)
 
             itemId?.let {
                 DbR.child(it).setValue(newItem).addOnCompleteListener {
                     Toast.makeText(applicationContext, "등록 되었습니다.", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, SellListActivity::class.java)
+                    startActivity(intent)
+
                 }
             }
         }
@@ -100,6 +114,7 @@ class AddItemActivity : AppCompatActivity() {
 
     companion object {
         private const val IMAGE_PICK_CODE = 1000
+        private const val PERMISSION_CODE = 1001;
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -120,7 +135,7 @@ class AddItemActivity : AppCompatActivity() {
                 .addOnSuccessListener {
                     storageRef.downloadUrl.addOnSuccessListener { downloadUri ->
                         val imageUrl = downloadUri.toString()
-                        addItemToDatabase(imageUrl)
+                        //addItemToDatabase(imageUrl)
                         // 이제 imageUrl을 Firebase Realtime Database에 저장하면 됩니다.
                     }
                 }
@@ -130,5 +145,34 @@ class AddItemActivity : AppCompatActivity() {
         }
     }
 
+
+    private fun requestStoragePermission() {
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+            PackageManager.PERMISSION_DENIED
+        ) {
+            // 권한이 거부되었다면 권한 요청
+            val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            requestPermissions(permissions, PERMISSION_CODE)
+        } else {
+            // 권한이 이미 있으면 이미지 선택
+            selectImage()
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 권한이 승인됨
+                    selectImage()
+                } else {
+                    // 권한이 거부됨
+                    Toast.makeText(this, "권한이 거부되었습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 
 }
